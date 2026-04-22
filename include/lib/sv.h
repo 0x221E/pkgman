9a7f8f1b99d8510fca4446dba3075998ca9f8075
0x221E <0x221E@0xinfinity.dev>
2026-04-22 21:09:00 +0200

    lib: sv: make SV() macro compile-time
    SV() no longer calls sv_create, instead initializes the struct
    in-place.

--- include/lib/sv.h
+++ include/lib/sv.h
@@ -12,6 +12,6 @@ struct string_view sv_create(char *buf, size_t len);
 struct string_view sv_copy(struct string_view *sv);
 int sv_equal(struct string_view *a, struct string_view *b);
 
-#define SV(x) sv_create((x), sizeof(x) - 1)
+#define SV(x) (struct string_view){(x), sizeof(x) - 1}
 
 #endif
