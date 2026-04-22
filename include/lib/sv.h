19d24559137697e683650dba099481abba7e703e
0x221E <0x221E@0xinfinity.dev>
2026-04-22 20:50:43 +0200

    lib: sv: add sv_equal() to sv.c and sv.h
    Optimized and bug-prone equality operation for string_view struct.
    This function will replace the equality checks operations between
    char* + string_view, and string_view + string_view.

--- include/lib/sv.h
+++ include/lib/sv.h
@@ -10,6 +10,7 @@ struct string_view {
 
 struct string_view sv_create(char *buf, size_t len);
 struct string_view sv_copy(struct string_view *sv);
+int sv_equal(struct string_view *a, struct string_view *b);
 
 #define SV(x) sv_create((x), sizeof(x) - 1)
 
--- src/lib/sv.c
+++ src/lib/sv.c
@@ -21,3 +21,20 @@ struct string_view sv_copy(struct string_view *sv)
     memcpy(copy.buf, sv->buf, sv->len);
     return copy;
 }
+
+int sv_equal(struct string_view *a, struct string_view *b)
+{
+    assert(a != NULL);
+    assert(b != NULL);
+
+    if (a->buf == NULL || b->buf == NULL)
+        return 0;
+
+    if (a->len != b->len)
+        return 0;
+
+    if (memcmp(a->buf, b->buf, a->len) != 0)
+        return 0;
+
+    return 1;
+}
